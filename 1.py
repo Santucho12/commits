f076226ab6e2b5c1c39245d664c443f499da242a
santucho12 <santysegal@gmail.com>
2025-01-09 13:20:17 -0300

    actualizando

--- 1.py
+++ 1.py
@@ -22,4 +22,4 @@ if __name__ == "__main__":
     print(persona2.to_json())
 #cual es el error?
 
-#porque necesita si o si el decordsadasasdsadadsadsadaasdSAssdad
+#porque necesita si o si el decordsadasasdsadadsadsadaasdSAssdaddasa
